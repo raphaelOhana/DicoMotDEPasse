a8d293d6050a37a9c1a3e7f79c538a22b091ce0f
reouven smadja <rusmadja@g.jct.ac.il>
2019-11-07 20:09:23 +0200

    tema mon algo tu me dis t en pense quoi !!!!

--- algoMdp/algoMdp/DicoMdp.h
+++ algoMdp/algoMdp/DicoMdp.h
@@ -1,21 +1,60 @@
 #pragma once
 #include<iostream>
+#include<time.h>
+#include<ctime>
+#include<string>
+#include<time.h>
+using namespace std;
 //prototype fonction
-int * creatPasseword(int n);
-void printPasseword(int n);
-char changeIntToChar(int x);
+//void CREATPASSEWORD(int n) { creatPasseword(n); }
 
+//-------------create one passeword and print it-----------------//
+void creatPasseword(int n /*give number of element in password*/);
+
+
+//-------------print one passeword-----------------//
+void printPasseword(int* n,int x);
+
+//-------------change int -> char whith own dictionary-----------------//
+//_________________A=10 .....Z=35 ,a=36....z=62_______________________//
+
+char changeIntToChar(int x/*number to cast*/);
+//_______________________________________________________________________//
+//____________________NE PAS TOUCHER PLEASE______________________________//
+//_______________________________________________________________________//
 
 char changeIntToChar(int x) 
 {
 	if (x < 10 || x>62)
 		throw "error 01";
-	if (x >= 10 || x<36)
+	if (x >= 10 && x<36)
 		return 'A' + x - 10;
-	if (x >= 36 || x < 63)
+	if (x >= 36 && x < 63)
 		return 'a' + x - 36;
 }
 
-void printPasseword(int n);
+void printPasseword(int* n,int x)
+{
+	for (int i = 0; i < x; i++)
+	{
+			if (n[i] >= 0 && n[i] < 10)
+				cout << n[i];
+			else
+				cout << changeIntToChar(n[i]);
+	}
+	cout << endl;
+}
 
-
+void creatPasseword(int n)
+{
+	int* charOfPasseword = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		
+		charOfPasseword[i] = rand() % 63;
+	}
+	printPasseword(charOfPasseword, n);
+}
+//_______________________________________________________________________//
+//_______________________________________________________________________//
+//_______________________________________________________________________//
--- algoMdp/algoMdp/Source.cpp
+++ algoMdp/algoMdp/Source.cpp
@@ -1,71 +1,74 @@
 #include<iostream>
 #include"DicoMdp.h"
 using namespace std;
+#define EX 2
+#if EX==1
 
-int main()
-{
-	char tab[8];
-
-	for (char i = 48; i < 122; i++)
+	int main()
 	{
-		if (i > 57 && i < 65)
-			i = 64 + i - 57;
-		else if(i > 90 && i < 97)
-			i = 96 + i - 90;
-		tab[0] = i;
-		for (char j = 48; j < 122; j++)
+		char tab[8];
+
+		for (char i = 48; i < 122; i++)
 		{
-			if (j > 57 && j < 65)
-				j = 64 + j - 57;
-			else if (j > 90 && j < 97)
-				j = 96 + j - 90;
-			tab[1] = j;
-			for (char k = 48; k < 122; k++)
+			if (i > 57 && i < 65)
+				i = 64 + i - 57;
+			else if (i > 90 && i < 97)
+				i = 96 + i - 90;
+			tab[0] = i;
+			for (char j = 48; j < 122; j++)
 			{
-				if (k > 57 && k < 65)
-					k = 64 + k - 57;
-				else if (k > 90 && k < 97)
-					k = 96 + k - 90;
-				tab[2] = k;
-				for (char l = 48; l < 122; l++)
+				if (j > 57 && j < 65)
+					j = 64 + j - 57;
+				else if (j > 90 && j < 97)
+					j = 96 + j - 90;
+				tab[1] = j;
+				for (char k = 48; k < 122; k++)
 				{
-					if (l > 57 && l < 65)
-						l = 64 + l - 57;
-					else if (l > 90 && l < 97)
-						l = 96 + l - 90;
-					tab[3] = l;
-					for (char m = 48; m < 122; m++)
+					if (k > 57 && k < 65)
+						k = 64 + k - 57;
+					else if (k > 90 && k < 97)
+						k = 96 + k - 90;
+					tab[2] = k;
+					for (char l = 48; l < 122; l++)
 					{
-						if (m > 57 && m < 65)
-							m = 64 + m - 57;
-						else if (m > 90 && m < 97)
-							m = 96 + m - 90;
-						tab[4] = m;
-						for (char n = 48; n < 122; n++)
+						if (l > 57 && l < 65)
+							l = 64 + l - 57;
+						else if (l > 90 && l < 97)
+							l = 96 + l - 90;
+						tab[3] = l;
+						for (char m = 48; m < 122; m++)
 						{
-							if (n > 57 && n < 65)
-								n = 64 + n - 57;
-							else if (n > 90 && n < 97)
-								n = 96 + n - 90;
-							tab[5] = n;
-							for (char o = 48; o < 122; o++)
+							if (m > 57 && m < 65)
+								m = 64 + m - 57;
+							else if (m > 90 && m < 97)
+								m = 96 + m - 90;
+							tab[4] = m;
+							for (char n = 48; n < 122; n++)
 							{
-								if (o > 57 && o < 65)
-									o = 64 + o - 57;
-								else if (o > 90 && o < 97)
-									o = 96 + o - 90;
-								tab[6] = o;
-								for (char p = 48; p < 122; p++)
+								if (n > 57 && n < 65)
+									n = 64 + n - 57;
+								else if (n > 90 && n < 97)
+									n = 96 + n - 90;
+								tab[5] = n;
+								for (char o = 48; o < 122; o++)
 								{
-									if (p > 57 && p < 65)
-										p = 64 + p - 57;
-									else if (p > 90 && p < 97)
-										p = 96 + p - 90;
-									tab[7] = p;
+									if (o > 57 && o < 65)
+										o = 64 + o - 57;
+									else if (o > 90 && o < 97)
+										o = 96 + o - 90;
+									tab[6] = o;
+									for (char p = 48; p < 122; p++)
+									{
+										if (p > 57 && p < 65)
+											p = 64 + p - 57;
+										else if (p > 90 && p < 97)
+											p = 96 + p - 90;
+										tab[7] = p;
 
-									cout << tab[0] << tab[1] << tab[2] << tab[3]
-										<< tab[4] << tab[5] << tab[6] << tab[7] << endl;
+										cout << tab[0] << tab[1] << tab[2] << tab[3]
+											<< tab[4] << tab[5] << tab[6] << tab[7] << endl;
 
+									}
 								}
 							}
 						}
@@ -73,6 +76,20 @@ int main()
 				}
 			}
 		}
+
 	}
 
-}
+#elif EX ==2
+
+	int main() 
+	{
+		//init number of passwords that you want
+		int numberOfPasseword = 200;
+		srand(time(NULL));
+		for (int i = 0; i < numberOfPasseword; i++)
+			creatPasseword(8);
+		
+		system("pause");
+	}
+
+#endif
